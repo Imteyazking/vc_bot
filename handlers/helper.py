b4a0bb2447339e77eff9bf2170e8a94543a55dd4
Imteyaz_king <79402181+Imteyazking@users.noreply.github.com>
2021-06-17 00:14:42 +0530

    Update helper.py

--- handlers/helper.py
+++ handlers/helper.py
@@ -4,7 +4,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_NAME as bn
 from helpers.filters import command, other_filters2, other_filters
 
-
+  Welcome to my  our Bot @KING_FIGHTER_MUSIC_BOT make in admin
+  Assistant manage vc @KING_FIGHTER_MUSIC_Ass Assistant ko make in admin only enjoy
 
 @Client.on_message(command("help") & other_filters2)
 async def helper(ok, message: Message):
@@ -12,17 +13,17 @@ async def helper(ok, message: Message):
         f"""💞 Hello! Following are the commands available for **{bn}** - __A Group Voice Chat Music Player__.
 The commands I currently support are:
 
-🎵 **Users Commands :**
-🌟 /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
-🌟 /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
-🌟 /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
-
+⚔️ **Users Commands :**
+⚔️ /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
+⚔️ /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
+⚔️ /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
+⚔️ /Repo   - ** [ Group & DM ]** > __github repo Imteyazking from DM Search
 
 🔰 **Admin & Sudo Users Commands :**
-🌟 /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
-🌟 /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
-🌟 /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
-🌟 /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
+⚔️ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
+⚔️ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
+⚔️ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
+⚔️ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
 
 @Client.on_message(command("help") & other_filters)
 async def ghelp(_, message: Message):
